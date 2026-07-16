aa96bbeae74a5a5cecc86cf143e8a06119ffb0d1
Ian Todd <todd.ian@gmail.com>
2026-07-16 20:30:27 +0800

    chore: Godot 4.7 .NET project skeleton + sim classlib + test cycle

---
+++ sim.Tests/PlaceholderTests.cs
@@ -0,0 +1,3 @@
+using GunsOnly.Sim;
+using Xunit;
+public class PlaceholderTests { [Fact] public void TwoIsTwo() => Assert.Equal(2, Placeholder.Two()); }
---
+++ sim/Placeholder.cs
@@ -0,0 +1,2 @@
+namespace GunsOnly.Sim;
+public static class Placeholder { public static int Two() => 2; }
