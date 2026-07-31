bb3341391dadf1fce969f743748cfa2b287af4df
Ian Todd <todd.ian@gmail.com>
2026-07-31 15:22:43 +0800

    Stamp Build 213

--- web/wwwroot/hud.js
+++ web/wwwroot/hud.js
@@ -52,7 +52,7 @@ import { hudPhasePresentation } from "./render/hud/hud_phase.js";
 import {
   armFlightAudio,
   setFlightAudioEnabled,
-} from "./render/audio/flight_audio.js?v=212";
+} from "./render/audio/flight_audio.js?v=213";
 
 const GREEN = "#4dff88";
 const GREEN_DIM = "rgba(77, 255, 136, 0.68)";
